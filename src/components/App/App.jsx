9ea1b323e11b5fdc1dc0c17ba00ffb37de879633
yurii-kovalchuk <sogdrums@gmail.com>
2023-07-14 13:13:22 +0300

    add form markup and styles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,7 @@ import { Form } from "../Form/Form";
 export const App = () => {
   return (
     <>
+      <h1>Test password strength</h1>
       <Form />
     </>
   );
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,6 +2,15 @@ import "./Form.style.css"
 
 export const Form = () => {
   return (
-    <div>Form</div>
+    <>
+      <form>
+        <input type="password" name="password" className="input" required />
+      </form>
+      <div className="wrap">
+        <div className="section"></div>
+        <div className="section"></div>
+        <div className="section"></div>
+      </div>
+    </>
   )
 }
